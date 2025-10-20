365b56750801fba44521e18b31758393b164c907
dearn1 <endra.sim@gmail.com>
2025-10-20 14:03:04 +1300

    created view for login, url for login and test.

--- message/urls.py
+++ message/urls.py
@@ -1,15 +1,17 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from message.views import send_message
+from message.views import send_message, LoginView
 from message.viewsets import Chat_roomViewSet, MessageViewSet
 
 router = DefaultRouter()
-
 router.register(r'chat_rooms', Chat_roomViewSet, basename='chat_rooms')
 router.register(r'message', MessageViewSet, basename='message')
+
 
 urlpatterns = [
     path('', include(router.urls)),
     path('send_message/', send_message, name='send_message'),
+    path('login/', LoginView.as_view(), name='login')
 ]
--- message_app/settings.py
+++ message_app/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'message',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -142,3 +143,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
